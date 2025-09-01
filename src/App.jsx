6162e3c9d51aa4173cc4fb52055259132312e5c6
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-01 16:09:02 +0200

    Importazione dell array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-
+const initialgenders =[
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
 function App() {
   return (
     <>
-      <h1>hello men</h1>
+      <div className="container">
+        
+      </div>
     </>
   )
 }
