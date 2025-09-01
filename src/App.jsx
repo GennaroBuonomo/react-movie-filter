8f701999a0dc7de5dc468cbaef248f90f36f2ad4
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-01 17:18:27 +0200

    creazione della select

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGenders =[
    { title: 'Inception', genre: 'Fantascienza' },
    { title: 'Il Padrino', genre: 'Thriller' },
@@ -7,6 +9,13 @@ const initialGenders =[
    { title: 'Pulp Fiction', genre: 'Thriller' },
  ]
 function App() {
+   const [newFilm, setNewFilm] = useState('');
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Scelta aggiornata!");
+   };
+ 
   return (
     <>
       <div className="container">
@@ -24,18 +33,31 @@ function App() {
                 })}
               </ul>
             </div>
-            <div className="col-12">
-              <form className="mt-4">
+            <div className="col-8">
+              <form onSubmit={handleSubmit} className="mt-4">
                 <div className="d-flex">
                   <input 
                   type="text"
                   className="form-control me-2"
-                  placeholder="Inserisci un titolo"                  
+                  placeholder="Inserisci un titolo" 
+                  value={newFilm}
+                  onChange={(e) => setNewFilm(e.target.value)}                 
                    />
                    <button className="btn btn-success">Inserisci</button>
                 </div>
               </form>
             </div>
+            <div className="col-4 mt-4">
+              <select class="form-select" aria-label="Default select example">
+               <option selected>Filtra per genere:</option>
+                <option>Fantascienza</option>
+                <option>Thriller</option>
+                <option>Romantico</option>
+                <option>Azione</option>
+                <option>Fantascienza</option>
+                <option>Thriller</option>
+              </select>
+           </div>
           </div>
         </div>
       </div>
